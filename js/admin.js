a56cb7c91cab531813de9aa7748db9ce55bb6108
hamiuking <61960758+hamiuking@users.noreply.github.com>
2026-02-11 10:14:57 +1300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -12,6 +12,7 @@ export function initAdminPage() {
   setupDriverActions();
   setupLedgerLookup();
   setupDashboardRefresh();
+  setupMarkPaid();
 }
 
 /* ---------------------------------------------------------
